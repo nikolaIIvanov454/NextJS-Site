77b8fd600022b9f010d242e36d90b19bf5b92f2e
Nikolai Ivanov <nikitoi_vanov@abv.bg>
2024-03-14 21:38:27 +0200

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LoginFormComponent from "../components/LoginForm"
+
+function RegisterPage() {
+  return (
+    <>
+      <LoginFormComponent />
+    </>
+  )
+}
+
+export default RegisterPage;
